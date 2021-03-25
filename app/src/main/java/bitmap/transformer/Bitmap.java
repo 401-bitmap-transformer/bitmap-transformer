e3c3ed1ccdb65bfa62e6a5600565c7d70d780c46
Ed <houedward@gmail.com>
2021-03-25 16:01:20 -0700

    added some methods to code up

--- app/src/main/java/bitmap/transformer/Bitmap.java
+++ app/src/main/java/bitmap/transformer/Bitmap.java
@@ -27,32 +27,19 @@ public class Bitmap {
     public void printPixels() {
         int w = this.data.getWidth();
         int h = this.data.getHeight();
-        int pixel = data.getRGB(0,h-1);
-
-        ColorModel colorModel = this.data.getColorModel();
-
-        System.out.printf("Red: %d, Green: %d, Blue %d\n",
-                colorModel.getRed(pixel),
-                colorModel.getGreen(pixel),
-                colorModel.getBlue(pixel));
-        //int[] rgb = new int[100];
-        //int[] pixels = this.data.getRGB(0, 0, w, h, rgb, 0, 0);
-    }
-
-    public void printPixels2() {
-        int w = this.data.getWidth();
-        int h = this.data.getHeight();
         Color pixel = new Color(data.getRGB(0,h-1));
-
-        ColorModel colorModel = this.data.getColorModel();
-
         System.out.printf("Red: %d, Green: %d, Blue %d\n",
                 pixel.getRed(),
                 pixel.getGreen(),
                 pixel.getBlue());
-        //int[] rgb = new int[100];
-        //int[] pixels = this.data.getRGB(0, 0, w, h, rgb, 0, 0);
+
+        //int[] pixels = this.data.getRGB(0, 0, w, h, null, 0, 1);
     }
 
+    public void makeGray(){
+    }
 
+    public void mirror(){
+
+    }
 }
